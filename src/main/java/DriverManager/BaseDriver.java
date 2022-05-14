35b6477e44541abeccc00389e05e6b6fd5ab6dbe
abhishekgupta <abhishekgupta183@gmail.com>
2022-05-14 16:27:42 +0800

    git commit "second commit"

--- src/main/java/DriverManager/BaseDriver.java
+++ src/main/java/DriverManager/BaseDriver.java
@@ -1,27 +1,19 @@
 package DriverManager;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.time.Duration;
 
 public class BaseDriver {
 
-    public static WebDriver driver;
 
-    public WebDriver init() {
-        System.setProperty("webdriver.chrome.driver", "/Users/abhishekgupta/Downloads/Downloads/chromedriver");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    // public static WebDriver driver;
 
-        return driver;
+    // public WebDriver init() {
+    // System.setProperty("webdriver.chrome.driver", "/Users/abhishekgupta/Downloads/Downloads/chromedriver");
+    //WebDriverManager.chromedriver().setup();
+    // driver = new ChromeDriver();
+    // driver.manage().window().maximize();
+    //  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-    }
-
-
-
-
+    // return driver;
 
 
 }
--- src/test/java/stepDef/HK01Step.java
+++ src/test/java/stepDef/HK01Step.java
@@ -1,23 +1,55 @@
 package stepDef;
 
-import StepsPages.HKcode;
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
 
 public class HK01Step {
+    IOSDriver iosDriver;
 
-    HKcode hKcode = new HKcode();
 
     @Given("chrome details available")
-    public void chromeDetailsAvailable() {
+    public void chromeDetailsAvailable() throws MalformedURLException, InterruptedException {
+
         System.out.println("Check the test first ...");
+        DesiredCapabilities desiredCaps = new DesiredCapabilities();
+        desiredCaps.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 12 Pro Max");
+        desiredCaps.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+        desiredCaps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "15.2");
+        desiredCaps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
+        desiredCaps.setCapability(MobileCapabilityType.APP, "/Users/abhishekgupta/Library/Developer/Xcode/DerivedData/WebDriverAgent-fxmsedpwfaalxlbcwefbceeuodem/Build/Products/Debug-iphonesimulator/IntegrationApp.app");
+
+
+        iosDriver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCaps);
+
+        System.out.println("success---------");
+
+        iosDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        iosDriver.findElement(AppiumBy.accessibilityId("Alerts")).click();
+
+        iosDriver.findElement(AppiumBy.accessibilityId("textField")).sendKeys("Hong Kong");
+        Thread.sleep(3000);
+
+        iosDriver.findElement(AppiumBy.xpath("//XCUIElementTypeStaticText[@name=\"Create App Alert\"]")).click();
+
+        iosDriver.switchTo().alert().accept();
+
+
     }
 
     @When("chrome driver open")
     public void chromeDriverOpen() {
         System.out.println("Check the test second  ...");
-        hKcode.openURL();
+        //hKcode.openURL();
     }
 
     @Then("check home page")
